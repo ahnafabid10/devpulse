06289d9d0917a7a9033df12645f443733875123b
Ahnaf Abid <ahnafabid600@gmail.com>
2026-06-24 00:19:59 +0600

    Fix routing setup in app.ts and ensure authRouter is exported correctly in auth.route.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { type Application, type Request, type Response } from 'express';
 import { userRouter } from './modules/user/user.route';
+import { issueRouter } from './modules/issues/issues.route';
+import { authRouter } from './modules/auth/auth.route';
 
 const app: Application = express();
 
@@ -20,5 +22,6 @@ app.get('/', (req: Request, res: Response) => {
 
 
 app.use('/api/auth', userRouter)
-
+app.use('/api/issue', issueRouter)
+app.use('/api/auth', authRouter)
 export default app
--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -5,3 +5,5 @@ const router = Router()
 
 router.post('/login', authController.loginUser)
 router.post("/refreshToken", authController.refreshToken)
+
+export const authRouter = router
